78d170b896072a1233c9317e36eee3bc940ddb01
tzarowski <tzarowski@users.noreply.github.com>
2018-07-19 16:17:05 +0100

    Dead Branch

--- TodoApi/Controllers/TodoController.cs
+++ TodoApi/Controllers/TodoController.cs
@@ -1,85 +1,85 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using System.Linq;
-using TodoApi.Models;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using System.Collections.Generic;
+//using System.Linq;
+//using TodoApi.Models;
 
-namespace TodoApi.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TodoController : ControllerBase
-    {
-        private readonly TodoContext _context;
+//namespace TodoApi.Controllers
+//{
+//    [Route("api/[controller]")]
+//    [ApiController]
+//    public class TodoController : ControllerBase
+//    {
+//        private readonly TodoContext _context;
 
-        public TodoController(TodoContext context)
-        {
-            _context = context;
+//        public TodoController(TodoContext context)
+//        {
+//            _context = context;
 
-            if (_context.TodoItems.Count() == 0)
-            {
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-                _context.TodoItems.Add(new TodoItem { Name = "Item2" });
-                _context.TodoItems.Add(new TodoItem { Name = "Item3" });
-                _context.TodoItems.Add(new TodoItem { Name = "Item4" });
-                _context.SaveChanges();
-            }
-        }
+//            if (_context.TodoItems.Count() == 0)
+//            {
+//                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+//                _context.TodoItems.Add(new TodoItem { Name = "Item2" });
+//                _context.TodoItems.Add(new TodoItem { Name = "Item3" });
+//                _context.TodoItems.Add(new TodoItem { Name = "Item4" });
+//                _context.SaveChanges();
+//            }
+//        }
 
-        [HttpGet]
-        public ActionResult<List<TodoItem>> GetAll()
-        {
-            return _context.TodoItems.ToList();
-        }
+//        [HttpGet]
+//        public ActionResult<List<TodoItem>> GetAll()
+//        {
+//            return _context.TodoItems.ToList();
+//        }
 
-        [HttpGet("{id}", Name = "GetTodo")]
-        public ActionResult<TodoItem> GetById(int id)
-        {
-            var item = _context.TodoItems.Find(id);
-            if (item == null)
-            {
-                return NotFound();
-            }
-            return item;
-        }
+//        [HttpGet("{id}", Name = "GetTodo")]
+//        public ActionResult<TodoItem> GetById(int id)
+//        {
+//            var item = _context.TodoItems.Find(id);
+//            if (item == null)
+//            {
+//                return NotFound();
+//            }
+//            return item;
+//        }
 
-        [HttpPost]
-        public IActionResult Create(TodoItem item)
-        {
-            _context.TodoItems.Add(item);
-            _context.SaveChanges();
+//        [HttpPost]
+//        public IActionResult Create(TodoItem item)
+//        {
+//            _context.TodoItems.Add(item);
+//            _context.SaveChanges();
 
-            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
-        }
+//            return CreatedAtRoute("GetTodo", new { id = item.Id }, item);
+//        }
 
-        [HttpPut("{id}")]
-        public IActionResult Update(int id, TodoItem item)
-        {
-            var todo = _context.TodoItems.Find(id);
-            if (todo == null)
-            {
-                return NotFound();
-            }
+//        [HttpPut("{id}")]
+//        public IActionResult Update(int id, TodoItem item)
+//        {
+//            var todo = _context.TodoItems.Find(id);
+//            if (todo == null)
+//            {
+//                return NotFound();
+//            }
 
-            todo.IsComplete = item.IsComplete;
-            todo.Name = item.Name;
+//            todo.IsComplete = item.IsComplete;
+//            todo.Name = item.Name;
 
-            _context.TodoItems.Update(todo);
-            _context.SaveChanges();
-            return NoContent();
-        }
+//            _context.TodoItems.Update(todo);
+//            _context.SaveChanges();
+//            return NoContent();
+//        }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var todo = _context.TodoItems.Find(id);
-            if (todo == null)
-            {
-                return NotFound();
-            }
+//        [HttpDelete("{id}")]
+//        public IActionResult Delete(int id)
+//        {
+//            var todo = _context.TodoItems.Find(id);
+//            if (todo == null)
+//            {
+//                return NotFound();
+//            }
 
-            _context.TodoItems.Remove(todo);
-            _context.SaveChanges();
-            return NoContent();
-        }
-    }
-}
+//            _context.TodoItems.Remove(todo);
+//            _context.SaveChanges();
+//            return NoContent();
+//        }
+//    }
+//}
